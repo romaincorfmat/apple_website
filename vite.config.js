0eadec0aad0f24555d05fc7b08fd2773576f8b49
serialfisher56 <112300122+noname20221@users.noreply.github.com>
2024-09-03 22:40:46 +0800

    implement 3D view oh the iphone

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), sentryVitePlugin({
+        org: "romain-corfmat",
+        project: "javascript-react",
+    }), sentryVitePlugin({
+        org: "romain-corfmat",
+        project: "javascript-nextjs",
+    }), sentryVitePlugin({
+        org: "romain-corfmat",
+        project: "javascript-react"
+    })],
+
+	build: {
+		sourcemap: true,
+	},
+});
